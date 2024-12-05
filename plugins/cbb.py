48dc04a3b240f0fb1c402d786b875c939b47dc2b
Riki <73009240+Royzzx@users.noreply.github.com>
2024-12-05 08:14:39 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text = f"<b>\n○ Channel : @MovieFive_App</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
